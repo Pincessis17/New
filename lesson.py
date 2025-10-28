1cae01699aaddce33531cc2beb706695819bea97
Princess <princessnhyiraaddai@gmail.com>
2025-10-28 06:12:00 +0400

    Headache

--- lesson.py
+++ lesson.py
@@ -1,13 +1,29 @@
-code = secret1234
-password =  input("Enter the password: ")
+code = "Secret123"
 while True:
-    if len(password)!=len(code):
+    password = input("Enter the password: ")
+    if len(password) != len(code):
         print("Incorrect password length. Try again.")
-    if len(password) == len(code) and password != code:
-        print("Password length is correct.But the password is incorrect. Try again.")
-    elif password == code:
+    elif len(password) == len(code) and password != code:
+        print("Password length is correct. But the password is incorrect. Try again.")
+    else:
         print("Access granted.")
         break
+
+        
+
+
+count = 1
+while count <= 10:
+    if count%3==0 and count%5==0:
+        print("FizzBuzz")
+    elif count % 3 == 0:
+        print("Fizz")
+    elif count%5 ==0:
+        print("Buzz")
+    elif count %3 !=0 and count %5 !=0:
+        print(count)
     else:
-        print("Incorrect password. Try again.")
+        print(count)
+    count += 1
+
 
